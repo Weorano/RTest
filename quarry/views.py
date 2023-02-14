da7c121fdc17ec4a5949a7223bababfdb5c057c6
Weorano <ignatov.roman.a@mail.ru>
2023-02-14 13:40:33 +0300

    reformat view as based view

--- quarry/views.py
+++ quarry/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from quarry.forms import ModelFilterForm
 from quarry.models import DumpTrucks
 
 
-def index_view(request):
-    parameters = DumpTrucks.objects.all()
+class IndexView(TemplateView):
+    template_name = 'index.html'
 
-    if request.method == 'GET':
-        form = ModelFilterForm(request.GET)
-        if not form.is_valid():
-            form.fields['model'].initial = 'Все'
-    else:
-        form = ModelFilterForm(request.GET)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
-    if form.is_valid():
-        parameters = DumpTrucks.objects.filter(model__name=form.cleaned_data['model'])
+        parameters = DumpTrucks.objects.all()
+        form = ModelFilterForm(self.request.GET)
 
-    return render(request, 'index.html', {
-        'form': form,
-        'parameters': parameters
-    })
+        if self.request.method == 'GET':
+            if not form.is_valid():
+                form.fields['model'].initial = 'Все'
+
+        if form.is_valid():
+            parameters = DumpTrucks.objects.filter(model__name=form.cleaned_data['model'])
+
+        context['form'] = form
+        context['parameters'] = parameters
+
+        return context
--- reksoft/urls.py
+++ reksoft/urls.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from quarry.views import index_view
+from quarry.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # -----------------------------------------------------------------------------------------------------------------
     # Основные страницы
     # -----------------------------------------------------------------------------------------------------------------
-    path('', index_view, name='Главная'),
+    path('', IndexView.as_view(), name='Главная'),
 ]
